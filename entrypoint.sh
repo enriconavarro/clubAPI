13b5722d2ef146f882483a6f46fd412a1ad914c2
Enrico Navarro <enricolimanavarro@gmail.com>
2020-11-09 18:21:23 -0300

    Corrigindo problemas no migrate do django.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 
+# waiting postgress
+sleep 30
+
+# run migrations
 python3 manage.py migrate --no-input >> /tmp/migrate_result
+
+# start server
 python3 manage.py runserver 0.0.0.0:80
